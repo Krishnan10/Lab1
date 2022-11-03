29a879c930926f81e6fa79ca9a76c0e12f069d79
Krishnan10 <116257537+Krishnan10@users.noreply.github.com>
2022-11-03 15:49:36 +0800

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,8 @@
+def calculate_bmi(height, weight):
+print("Height = " + str(height))
+print("Weight = " + str(weight))
+#Add code here to calculate BMI
+bmi =
+#Add code here to display calculate BMI
+print(. . .)
+calculate_bmi(weight=57, height=1.73)
